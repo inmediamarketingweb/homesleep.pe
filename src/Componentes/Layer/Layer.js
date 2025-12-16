8c52169f6e7c0e298d3509222fcabaf513401695
Inmedia 11 <inmedia.pc.11@gmail.com>
2025-12-16 10:53:53 -0500

    1.1.5

--- src/Componentes/Layer/Layer.js
+++ src/Componentes/Layer/Layer.js
@@ -3,7 +3,8 @@ import './Layer.css';
 function Layer(){
     return(
         <div className='page-layer'>
-            <img src="/assets/imagenes/SEO/logo-principal.jpg" alt=""/>
+            <img src="/assets/imagenes/SEO/logo-principal.jpg" alt="Logo principal de Homesleep"/>
+            <div class="layer-loader"></div>
         </div>
     )
 }
